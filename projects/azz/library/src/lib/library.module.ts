b1f2fb3d67f10faeab6044c44ac75e3799b1de0f
Fabrizio De Bortoli <f.debortoli@pillohealth.com>
2020-05-29 08:18:26 +0200

    Adding MatButtonModule

--- projects/azz/library/src/lib/library.module.ts
+++ projects/azz/library/src/lib/library.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [ConfirmDialogComponent],
-    imports: [],
-    exports: [ConfirmDialogComponent]
+    imports: [MatButtonModule],
+    exports: [ConfirmDialogComponent, MatButtonModule],
 })
 export class LibraryModule {}
