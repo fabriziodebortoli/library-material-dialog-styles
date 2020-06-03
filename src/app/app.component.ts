c1a7f51de60ca50bfe50473ff7fb747ab72827cf
Fabrizio De Bortoli <f.debortoli@pillohealth.com>
2020-06-03 12:19:00 +0200

    import @azz/library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ConfirmDialogModel, ConfirmDialogComponent } from 'projects/azz/library/src/lib/dialogs/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogModel, ConfirmDialogComponent } from '@azz/library';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
     constructor(private dialog: MatDialog) {}
@@ -17,10 +17,10 @@ export class AppComponent {
 
         const dialogRef = this.dialog.open(ConfirmDialogComponent, {
             maxWidth: '400px',
-            data: dialogData
+            data: dialogData,
         });
 
-        dialogRef.afterClosed().subscribe(dialogResult => {
+        dialogRef.afterClosed().subscribe((dialogResult) => {
             if (dialogResult) {
                 // this.notificationService.error('Error removing member...');
                 console.log('dialogResult YES', dialogResult);
